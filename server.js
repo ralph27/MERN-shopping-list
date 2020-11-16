5d000fe6ac3db848b51c5aa80b0dde9c6e2597db
ralph27 <maaloulyralph@gmail.com>
2020-11-16 23:19:02 +0200

    fixing server 2

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/items', items);
 // Serve Static Assets If In Production 
 if (process.env.NODE_ENV === 'production') {
    // Set static folder
-   app.use('/static', express.static(path.join(__dirname, 'client/build')));
+   app.use('/static', express.static(path.join(__dirname, 'client')))
 
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
